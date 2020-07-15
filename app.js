104608a61c36779e21255b68e236294b1b2e76ea
David Sparrius <david@mstone.com.au>
2020-07-15 18:41:13 +1000

    updated repo html object

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const util = require("util");
+
 const readTemplate = util.promisify(fs.readFile);
 const employees = [];
 
 
+var html = {
+header : '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><title>Team Profile</title></head><body><nav class="navbar navbar-dark bg-dark mb-5"><span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span></nav><div class="container"><div class="row">',
+engineer : '<div class="col-6"><div class="card mx-auto mb-3" style="width: 18rem"><h5 class="card-header">  "${name}" <br /><br />Engineer</h5><ul class="list-group list-group-flush"><li class="list-group-item">ID: ${id}</li><li class="list-group-item">Email Address: ${email}</li><li class="list-group-item">GitHub: ${gitHub}</li></ul></div></div>'        
+
+
+}
+
+// core object for adding, deleting and modifying member details
 function addMember() {
   inquirer
     .prompt([
@@ -73,16 +83,15 @@ function addMember() {
     });
 }
 
- async function getHtmlHeader() {
+async function getHtmlHeader() {
+  // load the text file html header content
+  const html = await readTemplate("./Assets/html.json");
 
-// load the text file html header content
-    const html = await readTemplate("./Assets/html.txt");
   fs.writeFile("./output/team.html", html, function (err) {
     if (err) {
       console.log(err);
     }
   });
- 
 }
 
 function addHtml(member) {
@@ -94,16 +103,10 @@ function addHtml(member) {
     let data = "";
     if (role === "Engineer") {
       const gitHub = member.getGithub();
-      data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
-            </ul>
-            </div>
-        </div>`;
+      data = '<div class="col-6"><div class="card mx-auto mb-3" style="width: 18rem"><h5 class="card-header">  "${name}" <br /><br />Engineer</h5><ul class="list-group list-group-flush"><li class="list-group-item">ID: ${id}</li><li class="list-group-item">Email Address: ${email}</li><li class="list-group-item">GitHub: ${gitHub}</li></ul></div></div>';
+
+
+
     } else if (role === "Intern") {
       const school = member.getSchool();
       data = `<div class="col-6">
@@ -154,9 +157,8 @@ function finishHtml() {
 }
 
 function init() {
-    getHtmlHeader();
-    addMember();
-  }
-
+  getHtmlHeader();
+  addMember();
+}
 
 init();
